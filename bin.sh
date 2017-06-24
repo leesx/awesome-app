e517ab03bfd786e026bc9941f5b8262c62ef23a0
lishangxi <lishangxi@hualala.com>
2017-06-24 18:02:54 +0800

    rebuild app

--- bin.sh
+++ bin.sh
@@ -7,10 +7,11 @@
 
 #!/bin/sh
 unset GIT_DIR
-DIR_ONE=/usr/local/works/awesome-app  #此目录为服务器页面展示目录
+DIR_ONE=/usr/local/works/awesome-app  #此目录为服务器页面展示目录  
 cd $DIR_ONE
 git init
 git remote add deploy  /usr/local/works/repos/awesome-bare.git
 git clean -df
 git pull deploy  master
+npm run debug:client
 #pm2 restart awesome-app  #pm2重启项目即可
