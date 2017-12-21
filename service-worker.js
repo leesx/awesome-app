5e64ca5b3b8b739174b1d3b9270b7c3720d3cf32
lishangxi <lishangxi@hualala.com>
2017-12-21 18:25:03 +0800

    add pwa

---
+++ service-worker.js
@@ -0,0 +1,154 @@
+// configuration
+
+const
+  version = '1.0.1',
+  CACHE = version + '::2014app',
+  offlineURL = '/offline/',
+  //一个拥有离线功能的页面必要文件的数组
+  installFilesEssential = [
+    '/',
+    '/manifest.json',
+    '/dist/scripts/offlinepage.js',
+    '/dist/images/logo/logo152.png'
+  ].concat(offlineURL),
+  //可选的，描述文件数组（installFilesDesirable）
+  installFilesDesirable = [
+    '/dist/images/logo/logo072.png',
+  ];
+
+// install static assets
+function installStaticFiles() {
+
+  return caches.open(CACHE)
+    .then(cache => {
+
+      // cache desirable files
+      cache.addAll(installFilesDesirable);
+
+      // cache essential files
+      return cache.addAll(installFilesEssential);
+
+    });
+
+}
+
+// clear old caches
+function clearOldCaches() {
+
+  return caches.keys()
+    .then(keylist => {
+
+      return Promise.all(
+        keylist
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      );
+
+    });
+
+}
+
+// application installation
+self.addEventListener('install', event => {
+
+  console.log('service worker: install');
+
+  // cache core files
+  event.waitUntil(
+    installStaticFiles()
+    .then(() => self.skipWaiting())
+  );
+
+});
+
+
+// application activated
+self.addEventListener('activate', event => {
+
+  console.log('service worker: activate');
+
+	// delete old caches
+  event.waitUntil(
+    clearOldCaches()
+    .then(() => self.clients.claim())
+	);
+
+});
+
+
+// is image URL?
+let iExt = ['png', 'jpg', 'jpeg', 'gif', 'webp', 'bmp'].map(f => '.' + f);
+function isImage(url) {
+
+  return iExt.reduce((ret, ext) => ret || url.endsWith(ext), false);
+
+}
+
+
+// return offline asset
+// 检查是否是一个图片请求，如果是，那么返回一个带有 “offline” 字样的 SVG。如果不是，返回 offlineURL 页面。
+function offlineAsset(url) {
+
+  if (isImage(url)) {
+
+    // return image
+    return new Response(
+      '<svg role="img" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title>offline</title><path d="M0 0h400v300H0z" fill="#eee" /><text x="200" y="150" text-anchor="middle" dominant-baseline="middle" font-family="sans-serif" font-size="50" fill="#ccc">offline</text></svg>',
+      { headers: {
+        'Content-Type': 'image/svg+xml',
+        'Cache-Control': 'no-store'
+      }}
+    );
+
+  }
+  else {
+
+    // return page
+    return caches.match(offlineURL);
+
+  }
+
+}
+
+
+// application fetch network data
+self.addEventListener('fetch', event => {
+
+  // abandon non-GET requests
+  if (event.request.method !== 'GET') return;
+
+  let url = event.request.url;
+
+  event.respondWith(
+
+    caches.open(CACHE)
+      .then(cache => {
+
+        return cache.match(event.request)
+          .then(response => {
+
+            if (response) {
+              // return cached file
+              console.log('cache fetch: ' + url);
+              return response;
+            }
+
+            // make network request
+            return fetch(event.request)
+              .then(newreq => {
+
+                console.log('network fetch: ' + url);
+                if (newreq.ok) cache.put(event.request, newreq.clone());
+                return newreq;
+
+              })
+              // app is offline
+              .catch(() => offlineAsset(url));
+
+          });
+
+      })
+
+  );
+
+});
